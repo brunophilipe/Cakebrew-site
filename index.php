1e493da71b0ee9c67e522645356b41d45c64e682
Bruno Philipe <brunoph.dev@gmail.com>
2014-04-29 23:48:51 -0300

    Initial commit.

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Cakebrew - The Mac App for Homebrew">
+	<meta name="author" content="Bruno Philipe - www.brunophilipe.com">
+	<link rel="shortcut icon" href="assets/img/favicon.png">
+
+	<title>Cakebrew</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="assets/css/main.css" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+</head>
+<body data-spy="scroll" data-offset="0" data-target="#navigation">
+
+	<a class="hidden-xs" href="https://github.com/brunophilipe/Cakebrew"><img style="position: fixed; top: 51px; left: 0; border: 0; width:141px; z-index:999;" src="assets/img/forkme.png" alt="Fork me on GitHub"></a>
+
+	<!-- Fixed navbar -->
+	<div id="navigation" class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/"><b>Cakebrew</b></a>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+			<li class="active"><a data-scroll href="#home" class="smothscroll">Home</a></li>
+			<li><a data-scroll href="#desc" class="smothscroll">Description</a></li>
+			<li><a data-scroll href="#showcase" class="smothScroll">Showcase</a></li>
+			<li><a data-scroll href="#contact" class="smothScroll">Contact</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+		</div>
+	</div>
+
+
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-12">
+					<h1 class="hidden-xs">Welcome to <b>Cakebrew</b><small>beta</small></h1>
+					<h1 class="visible-xs">Welcome to <b>Cakebrew</b><small>beta</small></h1>
+					<h3 class="hidden-xs">The Mac App for Homebrew.</h3>
+					<h3 class="visible-xs">The Mac App for Homebrew.</h3>
+					<br>
+				</div>
+
+				<div class="col-lg-2">
+					<h5>Useful</h5>
+					<p>Do most of the usual tasks you need of Homebrew. Including updating and finding problems with the Doctor!</p>
+					<img class="hidden-xs hidden-sm hidden-md arrow" src="assets/img/arrow1.png">
+				</div>
+				<div class="col-lg-8">
+					<img class="img-responsive" id="main-screenshot" src="assets/img/app-bg.png" alt="">
+				</div>
+				<div class="col-lg-2">
+					<br>
+					<h5>Practical &amp; Easy</h5>
+					<p>Too afraid to use the terminal but need that small program? Cakebrew is for you too!</p>
+					<img class="hidden-xs hidden-sm hidden-md arrow" src="assets/img/arrow2.png">
+				</div>
+			</div>
+		</div> <!--/ .container -->
+	</div><!--/ #headerwrap -->
+
+
+	<!-- INTRO WRAP -->
+	<section id="desc" name="desc"></section>
+	<div id="intro">
+		<div class="container">
+			<div class="row">
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title">Attention!</h3>
+					</div>
+					<div class="panel-body">
+						Apparently Cakebrew is very unstable on OS X versions 10.7 and 10.8. These issues are being fixed as they appear, and a stable version should be up in a couple of days.
+						In the meanwhile, please feel free to post issues on the <a href="https://github.com/brunophilipe/Cakebrew/issues">Cakebrew github page</a>.
+						<br>
+						Thanks for your patience.
+					</div>
+				</div>
+			</div>
+			<div class="row centered">
+				<h1>Designed To Be Awesome</h1>
+				<br>
+				<br>
+				<div class="col-lg-4">
+					<img src="assets/img/intro01.png" alt="">
+					<h3>Community Aware</h3>
+					<p>Cakebrew is open-source! Sweep through the code, fork, improve it as you wish!</p>
+				</div>
+				<div class="col-lg-4">
+					<img src="assets/img/intro02.png" alt="">
+					<h3>Easy to Use</h3>
+					<p>Cakebrew was designed with the user in mind. No complicated interfaces!</p>
+				</div>
+				<div class="col-lg-4">
+					<img src="assets/img/intro03.png" alt="">
+					<h3>King of the Forest</h3>
+					<p>Cakebrew is compatible with all OS X versions back to OS X 10.7 Lion.</p>
+				</div>
+			</div>
+			<br>
+			<hr>
+		</div> <!--/ .container -->
+	</div><!--/ #introwrap -->
+
+	<!-- FEATURES WRAP -->
+	<div class="features">
+		<div class="container">
+			<div class="col-lg-8 col-md-7">
+				<div class="row">
+					<h1 class="centered">Download</h1>
+					<br>
+					<p class="text-center"><big>Download Cakebrew today and discover the wonderful world of Homebrew and its Formulas!</big><br>Current Version: 1.0.2</p>
+					<br>
+					<p class="text-center">
+						<a href="http://www.cakebrew.com/files/cakebrew-1.0.2.dmg" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-cloud-download"></span>Download Now</a>
+						<a href="https://github.com/brunophilipe/Cakebrew/releases/tag/v1.0.2" class="btn btn-lg btn-warning"><span class="glyphicon glyphicon-folder-open"></span>Get Source <small>on GitHub</small></a>
+					</p>
+					<p><small>Notice: Since Cakebrew is not distributed using an Apple Developer Account, OS X may not launch it, saying it "Can't be opened because it is from an unidentified developer". To open Cakebrew, all you have to do is right click the App, click open and click open again in the confirmation dialog. Thanks for your patience!</small></p>
+				</div>
+				<br>
+				<hr>
+				<div class="row">
+					<h1 class="centered">Installation Notice</h1>
+					<br>
+					<p class="text-center">
+						<big>It is important to notice that Cakebrew doesn't install Homebrew for you. To install Homebrew, please visit their website.</big>
+						<br>
+						<small>Installing Homebrew though Cakebrew is currently in the To-Do List =)</small>
+					</p>
+					<br>
+					<p class="text-center">
+						<a href="http://brew.sh" class="btn btn-lg btn-success" target="_blank"><span class="glyphicon glyphicon-star-empty"></span>Homebrew Website</a>
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-lg-offset-1 col-md-offset-1 col-md-4 well">
+				<div class="row">
+					<h1 class="centered">To-Do List</h1>
+					<ul id="todo">
+						<li><p><b>Homebrew installation.</b> Allow user to install Homebrew from within Cakebrew.</p></li>
+						<li><p><b>Support Homebrew localizations!</b> Currently only English is supported.<br>The data parser currently relies on the "==> Dependencies" and other similar strings.</p></li>
+						<li><p><b>Improve Doctor.</b> Currently only displays output after task is done. Would be better to do it live like in the terminal.</p></li>
+						<li><p>And much more!</p></li>
+					</ul>
+				</div>
+			</div>
+			<br>
+		</div><!--/ .container -->
+	</div><!--/ #features -->
+
+	<section id="showcase" name="showcase"></section>
+	<div id="showcase">
+		<div class="container">
+			<div class="row">
+				<h1 class="centered">More Screenshots</h1>
+				<br>
+				<div class="col-lg-12">
+					<div id="carousel-example-generic" class="carousel slide">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="assets/img/item-01.png" alt="">
+							</div>
+							<div class="item">
+								<img src="assets/img/item-02.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<br>
+			<br>
+		</div><!-- /container -->
+	</div>
+
+
+	<section id="contact" name="contact"></section>
+	<div id="footerwrap">
+		<div class="container">
+			<div class="col-lg-8 col-md-7">
+				<h3>Cakebrew Maintainer</h3>
+				<p>
+					<b>Bruno Philipe</b><br>
+					<small>Brasilia, Brazil &mdash; <a href="http://www.brunophilipe.com/">Website</a> | <a href="mailto:hello@brunophilipe.com">Email</a></small>
+				</p>
+				<p>
+					<b>Wanna become a maintainer?</b><br>
+					<small><a href="http://www.brunophilipe.com/">Fork Cakebrew on GitHub!</a></small>
+				</p>
+				<hr>
+				<p><small><b>
+					Cakebrew supports all versions of OS X back to version 10.7.
+					<br>
+					Cakebrew is written in Objective-C and compiled in Xcode 5.1.1 on OS 10.9.2.
+				</b></small></p>
+			</div>
+
+			<div class="col-lg-4 col-md-5">
+				<h3>Drop Us A Line</h3>
+				<br>
+				<form role="form" action="/mailme.php" method="post" enctype="plain">
+					<div class="form-group">
+						<label for="name1">Your Name</label>
+						<input type="name" name="name" class="form-control" id="name1" placeholder="Your Name" required>
+					</div>
+					<div class="form-group">
+						<label for="email1">Your Email</label>
+						<input type="email" name="email" class="form-control" id="email1" placeholder="Enter email" required>
+					</div>
+					<div class="form-group">
+						<label>Your Message</label>
+						<textarea class="form-control" name="text" rows="3" placeholder="Please type your message here..." required></textarea>
+					</div>
+					<br>
+					<button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="c">
+		<div class="container">
+			<div class="col-lg-7"><p>Cakebrew &copy; Bruno Philipe 2014<br><small>Licensed under GNU GPL v3 License - All Rights Reserved</small><br><span id="retina">This website is 100% compatible with Retina Screen Macs</span></p></div>
+			<div class="col-lg-5 text-right"><p>Website Created by <a href="http://www.brunophilipe.com/">Bruno Philipe</a><br>Theme by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p></div>
+		</div>
+	</div>
+
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/smooth-scroll.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script>
+	$('.carousel').carousel({
+		interval: 3500
+	})
+
+	smoothScroll.init({
+		speed: 500, // Integer. How fast to complete the scroll in milliseconds
+		easing: 'easeInOutCubic', // Easing pattern to use
+		updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+		offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+		callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
+		callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
+	});
+	</script>
+	<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+	</body>
+</html>
---
+++ mailme.php
@@ -0,0 +1,148 @@
+<?php
+
+$crdate = date("r");
+
+$status = (($_POST['name'] != "") && ($_POST['email'] != "") && ($_POST['text'] != ""));
+
+if ($status) {
+	$email_s_name		= $_POST['name'];
+	$email_s_email		= $_POST['email'];
+	$email_s_message	= $_POST['text'];
+
+	$email_to			= "Bruno Philipe <brunophilipe@zoho.com>";
+	$email_subject		= "Message sent from Cakebrew contact form.";
+	$email_from			= "From: Cakebrew Contact Form <mailme@cakebrew.com>\r\nContent-Type: text/html\r\nReply-To: $email_s_name <$email_s_email>\r\n";
+	$email_message		=
+
+	"<!DOCTYPE html><html><head><title>$email_subject</title>
+	<style type='text/css'>
+	div#message {font-family: \"Lucida Console\", Monaco, monospace;}
+	body {font-family: \"Trebuchet MS\", Helvetica, sans-serif;}
+	p#textarea {white-space: pre-line;}
+	p#footer {font-size:9;}
+	</style></head><body>
+	<p>An user has contacted you trough the Cakebrew contact form.</p>
+	<p>This message was created at $crdate.</p>
+	<p>User information:</p>
+	<ul>
+		<li>Name: $email_s_name</li>
+		<li>Email: $email_s_email</li>
+	</ul>
+	<div id='message'>
+	<p>
+	-------------------<br />
+	User message begins<br />
+	-------------------<br />
+	</p>
+	<p id='textarea'>
+	$email_s_message
+	</p>
+	<p>
+	-----------------<br />
+	User message ends<br />
+	-----------------<br />
+	</p>
+	</div>
+	Best Regards.<br />
+	<p id='footer'>MailMe Contact Form by Bruno Philipe</p>
+	</body>
+	</html>
+	";
+
+	$status = mail($email_to,$email_subject,$email_message,$email_from);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Cakebrew - The Mac App for Homebrew">
+	<meta name="author" content="Bruno Philipe - www.brunophilipe.com">
+	<link rel="shortcut icon" href="assets/img/favicon.png">
+
+	<title>Cakebrew - Contact</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="assets/css/main.css" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+</head>
+<body data-spy="scroll" data-offset="0" data-target="#navigation">
+
+	<!-- Fixed navbar -->
+	<div id="navigation" class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><b>Cakebrew</b></a>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="/" class="smothscroll">Home</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+		</div>
+	</div>
+
+	<!-- FEATURES WRAP -->
+	<div class="features">
+		<div class="container">
+			<div class="row">
+				<h1 class="centered">Contact</h1>
+				<br>
+				<?php if ($status): ?>
+				<p class="text-center"><big>Thanks for the message. If you are waiting for a reply, please be patient.</big></p>
+				<?php else: ?>
+				<p class="text-center"><big>There was an error sending the email. Please be sure you filled ALL the form fields. To try again click the Back button in your browser.</big></p>
+				<?php endif; ?>
+				<br>
+				<p class="text-center">
+					<a href="http://www.cakebrew.com/" class="btn btn-lg btn-primary">Go back to Homepage</a>
+				</p>
+			</div>
+			<br>
+		</div><!--/ .container -->
+	</div><!--/ #features -->
+
+	<div id="c">
+		<div class="container">
+			<div class="col-lg-7"><p>Cakebrew &copy; Bruno Philipe 2014<br><small>Licensed under GNU GPL v3 License - All Rights Reserved</small><br><span id="retina">This website is 100% compatible with Retina Screen Macs</span></p></div>
+			<div class="col-lg-5 text-right"><p>Website Created by <a href="http://www.brunophilipe.com/">Bruno Philipe</a><br>Theme by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p></div>
+		</div>
+	</div>
+
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/smooth-scroll.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script>
+	$('.carousel').carousel({
+		interval: 3500
+	})
+
+	smoothScroll.init({
+		speed: 500, // Integer. How fast to complete the scroll in milliseconds
+		easing: 'easeInOutCubic', // Easing pattern to use
+		updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+		offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+		callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
+		callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
+	});
+	</script>
+	<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+	</body>
+</html>
