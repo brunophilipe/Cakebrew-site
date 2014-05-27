823afde5f0d752f051523a54ba234ac2403c89bd
Bruno Philipe <hello@brunophilipe.com>
2014-05-27 20:05:39 -0300

    Added GitHub buttons.

--- index.php
+++ index.php
@@ -107,6 +107,11 @@
 				*/ ?>
 			</div>
 			<div class="row centered">
+				<iframe src="http://ghbtns.com/github-btn.html?user=brunophilipe&repo=Cakebrew&type=follow" allowtransparency="true" frameborder="0" scrolling="0" width="162" height="20"></iframe>
+				<iframe src="http://ghbtns.com/github-btn.html?user=brunophilipe&repo=Cakebrew&type=fork&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="20"></iframe>
+				<iframe src="http://ghbtns.com/github-btn.html?user=brunophilipe&repo=Cakebrew&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="20"></iframe>
+				<br>
+				<br>
 				<h1>Designed To Be Awesome</h1>
 				<br>
 				<br>
