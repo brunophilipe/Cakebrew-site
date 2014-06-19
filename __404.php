3fee151c7d32f08775c1b3d714297d4193b5438a
Bruno Philipe <hello@brunophilipe.com>
2014-06-19 01:59:43 -0300

    Fixed problem in 404 page.

--- __404.php
+++ __404.php
@@ -40,7 +40,7 @@
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
-				<li><a href="https://www.cakebrew.com"Home</a></li>
+				<li><a href="https://www.cakebrew.com">Home</a></li>
 			</ul>
 		</div><!--/.nav-collapse -->
 		</div>
