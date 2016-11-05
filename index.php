56352415aca1cc5b462205bb5a9029ddeaa9c27c
Bruno Philipe <brunophilipe@me.com>
2016-11-06 00:42:12 +0100

    Updates links to new version

--- index.php
+++ index.php
@@ -145,14 +145,14 @@
 				<div class="row">
 					<h1 class="centered">Download</h1>
 					<br>
-					<p class="text-center"><span class="big">Download Cakebrew today and discover the wonderful world of Homebrew and its Formulae!</span><br>Current Version: 1.2.2 (Nov 1st, 2016)</p>
+					<p class="text-center"><span class="big">Download Cakebrew today and discover the wonderful world of Homebrew and its Formulae!</span><br>Current Version: 1.2.3 (Nov 6th, 2016)</p>
 					<br>
 					<p class="text-center btns-download">
-						<a href="https://cdn.cakebrew.com/cakebrew-1.2.2.dmg" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-cloud-download"></span>Download Now <small>via KeyCDN</small></a>
+						<a href="https://cdn.cakebrew.com/cakebrew-1.2.3.dmg" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-cloud-download"></span>Download Now <small>via KeyCDN</small></a>
 						<a href="https://github.com/brunophilipe/Cakebrew/releases" class="btn btn-lg btn-warning"><span class="glyphicon glyphicon-folder-open"></span>Get Source <small>on GitHub</small></a>
 					</p>
 					<p class="text-center">
-						<a class="btn btn-sm btn-default" href="https://www.cakebrew.com/files/cakebrew-1.2.2.dmg">Direct Download</a>
+						<a class="btn btn-sm btn-default" href="https://www.cakebrew.com/files/cakebrew-1.2.3.dmg">Direct Download</a>
 					</p>
 				</div>
 				<hr>
